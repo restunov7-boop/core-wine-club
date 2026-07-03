ee2d09daf686bf65e2a03cc54223fd92fe30be02
restunov7-boop <restunov7@yandex.ru>
2026-07-03 21:33:23 +0300

    Sprint 33 expand wine suggestions

--- frontend/src/modules/diary/data/wineSuggestions.ts
+++ frontend/src/modules/diary/data/wineSuggestions.ts
@@ -6,7 +6,15 @@ export type WineSuggestion = {
   style?: string;
 };
 
-export const wineSuggestions: WineSuggestion[] = [
+type WineSuggestionGroup = {
+  country: string;
+  regions: string[];
+  grapes: string[];
+  stylesByGrape?: Record<string, string>;
+  defaultStyle: string;
+};
+
+const baseSuggestions: WineSuggestion[] = [
   { name: "Prosecco Valdobbiadene", country: "Italy", region: "Veneto", grape: "Glera", style: "Sparkling" },
   { name: "Champagne Brut", country: "France", region: "Champagne", grape: "Chardonnay / Pinot Noir / Meunier", style: "Sparkling" },
   { name: "Cremant de Bourgogne", country: "France", region: "Burgundy", grape: "Chardonnay / Pinot Noir", style: "Sparkling" },
@@ -57,6 +65,229 @@ export const wineSuggestions: WineSuggestion[] = [
   { name: "Mukuzani", country: "Georgia", region: "Kakheti", grape: "Saperavi", style: "Red" },
   { name: "Kindzmarauli", country: "Georgia", region: "Kakheti", grape: "Saperavi", style: "Red semi-sweet" },
   { name: "Khvanchkara", country: "Georgia", region: "Racha", grape: "Aleksandrouli / Mujuretuli", style: "Red semi-sweet" },
-  { name: "Tokaji Late Harvest", country: "Hungary", region: "Tokaj", grape: "Furmint", style: "Semi-sweet" },
+  { name: "Tokaji Late Harvest", country: "Hungary", region: "Tokaj", grape: "Furmint", style: "Dessert" },
   { name: "Lambrusco Emilia", country: "Italy", region: "Emilia-Romagna", grape: "Lambrusco", style: "Sparkling red" },
+  { name: "Port Ruby", country: "Portugal", region: "Douro", grape: "Touriga Nacional / Tinta Roriz", style: "Fortified" },
+  { name: "Sherry Fino", country: "Spain", region: "Jerez", grape: "Palomino", style: "Fortified" },
+  { name: "Madeira Medium Dry", country: "Portugal", region: "Madeira", grape: "Sercial / Verdelho", style: "Fortified" },
 ];
+
+const wineSuggestionGroups: WineSuggestionGroup[] = [
+  {
+    country: "France",
+    regions: ["Bordeaux", "Medoc", "Saint-Emilion", "Pomerol", "Graves"],
+    grapes: ["Merlot", "Cabernet Sauvignon", "Cabernet Franc", "Sauvignon Blanc", "Semillon"],
+    defaultStyle: "Red",
+    stylesByGrape: { "Sauvignon Blanc": "White", Semillon: "White" },
+  },
+  {
+    country: "France",
+    regions: ["Burgundy", "Chablis", "Cote de Beaune", "Cote de Nuits", "Maconnais"],
+    grapes: ["Chardonnay", "Pinot Noir", "Aligote", "Gamay"],
+    defaultStyle: "Red",
+    stylesByGrape: { Chardonnay: "White", Aligote: "White" },
+  },
+  {
+    country: "France",
+    regions: ["Loire Valley", "Sancerre", "Anjou", "Vouvray", "Muscadet"],
+    grapes: ["Sauvignon Blanc", "Chenin Blanc", "Cabernet Franc", "Melon de Bourgogne", "Pinot Noir"],
+    defaultStyle: "White",
+    stylesByGrape: { "Cabernet Franc": "Red", "Pinot Noir": "Red" },
+  },
+  {
+    country: "France",
+    regions: ["Rhone Valley", "Cote Rotie", "Hermitage", "Chateauneuf-du-Pape", "Tavel"],
+    grapes: ["Syrah", "Grenache", "Marsanne", "Roussanne", "Grenache / Cinsault"],
+    defaultStyle: "Red",
+    stylesByGrape: { Marsanne: "White", Roussanne: "White", "Grenache / Cinsault": "Rose" },
+  },
+  {
+    country: "France",
+    regions: ["Alsace", "Provence", "Languedoc", "Roussillon"],
+    grapes: ["Riesling", "Gewurztraminer", "Pinot Gris", "Grenache", "Carignan", "Cinsault"],
+    defaultStyle: "White",
+    stylesByGrape: { Grenache: "Red", Carignan: "Red", Cinsault: "Rose" },
+  },
+  {
+    country: "Italy",
+    regions: ["Tuscany", "Chianti", "Montalcino", "Montepulciano", "Bolgheri"],
+    grapes: ["Sangiovese", "Merlot", "Cabernet Sauvignon", "Vermentino", "Trebbiano"],
+    defaultStyle: "Red",
+    stylesByGrape: { Vermentino: "White", Trebbiano: "White" },
+  },
+  {
+    country: "Italy",
+    regions: ["Piedmont", "Barolo", "Barbaresco", "Langhe", "Asti"],
+    grapes: ["Nebbiolo", "Barbera", "Dolcetto", "Moscato", "Cortese"],
+    defaultStyle: "Red",
+    stylesByGrape: { Moscato: "Dessert", Cortese: "White" },
+  },
+  {
+    country: "Italy",
+    regions: ["Veneto", "Valpolicella", "Soave", "Friuli", "Alto Adige"],
+    grapes: ["Corvina", "Garganega", "Pinot Grigio", "Sauvignon Blanc", "Ribolla Gialla"],
+    defaultStyle: "White",
+    stylesByGrape: { Corvina: "Red" },
+  },
+  {
+    country: "Italy",
+    regions: ["Sicily", "Etna", "Sardinia", "Puglia", "Emilia-Romagna"],
+    grapes: ["Nero d'Avola", "Nerello Mascalese", "Vermentino", "Primitivo", "Lambrusco"],
+    defaultStyle: "Red",
+    stylesByGrape: { Vermentino: "White", Lambrusco: "Sparkling red" },
+  },
+  {
+    country: "Spain",
+    regions: ["Rioja", "Ribera del Duero", "Toro", "Priorat", "Navarra", "Jumilla"],
+    grapes: ["Tempranillo", "Garnacha", "Monastrell", "Graciano", "Viura"],
+    defaultStyle: "Red",
+    stylesByGrape: { Viura: "White" },
+  },
+  {
+    country: "Spain",
+    regions: ["Rias Baixas", "Rueda", "Penedes", "Catalonia", "Jerez"],
+    grapes: ["Albarino", "Verdejo", "Xarel-lo", "Macabeo", "Palomino"],
+    defaultStyle: "White",
+    stylesByGrape: { "Xarel-lo": "Sparkling", Macabeo: "Sparkling", Palomino: "Fortified" },
+  },
+  {
+    country: "Portugal",
+    regions: ["Douro", "Dao", "Alentejo", "Vinho Verde", "Madeira"],
+    grapes: ["Touriga Nacional", "Touriga Franca", "Tinta Roriz", "Alvarinho", "Arinto", "Verdelho"],
+    defaultStyle: "Red",
+    stylesByGrape: { Alvarinho: "White", Arinto: "White", Verdelho: "Fortified" },
+  },
+  {
+    country: "Germany",
+    regions: ["Mosel", "Rheingau", "Pfalz", "Rheinhessen"],
+    grapes: ["Riesling", "Spatburgunder", "Silvaner", "Gewurztraminer", "Muller-Thurgau"],
+    defaultStyle: "White",
+    stylesByGrape: { Spatburgunder: "Red" },
+  },
+  {
+    country: "Austria",
+    regions: ["Wachau", "Kamptal", "Kremstal", "Burgenland"],
+    grapes: ["Gruner Veltliner", "Riesling", "Zweigelt", "Blaufrankisch", "Welschriesling"],
+    defaultStyle: "White",
+    stylesByGrape: { Zweigelt: "Red", Blaufrankisch: "Red" },
+  },
+  {
+    country: "Georgia",
+    regions: ["Kakheti", "Kartli", "Imereti", "Racha"],
+    grapes: ["Saperavi", "Rkatsiteli", "Kisi", "Mtsvane", "Tsolikouri", "Aleksandrouli"],
+    defaultStyle: "Orange",
+    stylesByGrape: { Saperavi: "Red", Aleksandrouli: "Red semi-sweet" },
+  },
+  {
+    country: "Greece",
+    regions: ["Santorini", "Naoussa", "Nemea", "Crete"],
+    grapes: ["Assyrtiko", "Xinomavro", "Agiorgitiko", "Vidiano", "Moschofilero"],
+    defaultStyle: "White",
+    stylesByGrape: { Xinomavro: "Red", Agiorgitiko: "Red" },
+  },
+  {
+    country: "Moldova",
+    regions: ["Codru", "Valul lui Traian", "Stefan Voda", "Purcari"],
+    grapes: ["Feteasca Alba", "Feteasca Regala", "Feteasca Neagra", "Rara Neagra", "Viorica"],
+    defaultStyle: "White",
+    stylesByGrape: { "Feteasca Neagra": "Red", "Rara Neagra": "Red" },
+  },
+  {
+    country: "Armenia",
+    regions: ["Areni", "Vayots Dzor", "Armavir", "Aragatsotn"],
+    grapes: ["Areni Noir", "Voskehat", "Kangun", "Karmrahyut"],
+    defaultStyle: "Red",
+    stylesByGrape: { Voskehat: "White", Kangun: "White" },
+  },
+  {
+    country: "USA",
+    regions: ["Napa Valley", "Sonoma Coast", "Russian River Valley", "Willamette Valley", "Lodi", "Walla Walla"],
+    grapes: ["Cabernet Sauvignon", "Merlot", "Pinot Noir", "Chardonnay", "Zinfandel", "Syrah"],
+    defaultStyle: "Red",
+    stylesByGrape: { Chardonnay: "White" },
+  },
+  {
+    country: "Chile",
+    regions: ["Maipo Valley", "Colchagua Valley", "Casablanca Valley", "Aconcagua Valley", "Maule Valley"],
+    grapes: ["Cabernet Sauvignon", "Carmenere", "Merlot", "Sauvignon Blanc", "Chardonnay", "Pais"],
+    defaultStyle: "Red",
+    stylesByGrape: { "Sauvignon Blanc": "White", Chardonnay: "White" },
+  },
+  {
+    country: "Argentina",
+    regions: ["Mendoza", "Uco Valley", "Salta", "Patagonia", "San Juan"],
+    grapes: ["Malbec", "Bonarda", "Cabernet Sauvignon", "Torrontes", "Chardonnay"],
+    defaultStyle: "Red",
+    stylesByGrape: { Torrontes: "White", Chardonnay: "White" },
+  },
+  {
+    country: "South Africa",
+    regions: ["Stellenbosch", "Swartland", "Franschhoek", "Walker Bay"],
+    grapes: ["Chenin Blanc", "Sauvignon Blanc", "Chardonnay", "Pinotage", "Syrah", "Cabernet Sauvignon"],
+    defaultStyle: "White",
+    stylesByGrape: { Pinotage: "Red", Syrah: "Red", "Cabernet Sauvignon": "Red" },
+  },
+  {
+    country: "Australia",
+    regions: ["Barossa Valley", "Margaret River", "Yarra Valley", "Hunter Valley", "Clare Valley"],
+    grapes: ["Shiraz", "Cabernet Sauvignon", "Chardonnay", "Riesling", "Semillon", "Grenache"],
+    defaultStyle: "Red",
+    stylesByGrape: { Chardonnay: "White", Riesling: "White", Semillon: "White" },
+  },
+  {
+    country: "New Zealand",
+    regions: ["Marlborough", "Central Otago", "Hawke's Bay", "Martinborough", "Gisborne"],
+    grapes: ["Sauvignon Blanc", "Pinot Noir", "Chardonnay", "Riesling", "Pinot Gris"],
+    defaultStyle: "White",
+    stylesByGrape: { "Pinot Noir": "Red" },
+  },
+  {
+    country: "Hungary",
+    regions: ["Tokaj", "Eger", "Villany", "Balaton"],
+    grapes: ["Furmint", "Harslevelu", "Kekfrankos", "Kadarka", "Muscat"],
+    defaultStyle: "White",
+    stylesByGrape: { Kekfrankos: "Red", Kadarka: "Red", Muscat: "Dessert" },
+  },
+  {
+    country: "Uruguay",
+    regions: ["Canelones", "Maldonado", "Colonia"],
+    grapes: ["Tannat", "Albarino", "Merlot", "Cabernet Franc"],
+    defaultStyle: "Red",
+    stylesByGrape: { Albarino: "White" },
+  },
+];
+
+export const wineSuggestions: WineSuggestion[] = buildWineSuggestions();
+
+function buildWineSuggestions(): WineSuggestion[] {
+  const suggestions = [...baseSuggestions];
+
+  for (const group of wineSuggestionGroups) {
+    for (const region of group.regions) {
+      for (const grape of group.grapes) {
+        suggestions.push({
+          name: `${region} ${grape}`,
+          country: group.country,
+          region,
+          grape,
+          style: group.stylesByGrape?.[grape] ?? group.defaultStyle,
+        });
+      }
+    }
+  }
+
+  return uniqueSuggestions(suggestions).slice(0, 500);
+}
+
+function uniqueSuggestions(suggestions: WineSuggestion[]): WineSuggestion[] {
+  const seen = new Set<string>();
+
+  return suggestions.filter((suggestion) => {
+    const key = `${suggestion.name}|${suggestion.country}|${suggestion.region}|${suggestion.grape}`.toLowerCase();
+    if (seen.has(key)) {
+      return false;
+    }
+    seen.add(key);
+    return true;
+  });
+}
